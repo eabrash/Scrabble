45bd0f380d7c68ebcb21c4c2e2b536e432d5f506
Erin Bond <erin@erinebond.com>
2016-08-30 14:35:24 -0700

    Modified the play method

--- lib/Scrabble_player.rb
+++ lib/Scrabble_player.rb
@@ -7,6 +7,7 @@ attr_reader :name
   def initialize(name)
     @name = name
     @plays = []
+    @won = false
   end
 #plays: returns an Array of the words played by the player
 
@@ -14,17 +15,23 @@ attr_reader :name
     return @plays
   end
 
+#play(word): Adds the input word to the plays Array
+# Returns false if player has already won
+# Returns the score of the word
   def play(word)
-    @plays << word
+    unless @won == true
+       @plays << word
+       return Scrabble::Scoring.score(word)
+    else
+      return false
+    end
   end
 
 end
 
 
-#plays: returns an Array of the words played by the player
-#play(word): Adds the input word to the plays Array
-# Returns false if player has already won
-# Returns the score of the word
+
+
 #total_score: Returns the sum of scores of played words
 #won?: If the player has over 100 points, returns true, otherwise returns false
 #highest_scoring_word: Returns the highest scoring played word
