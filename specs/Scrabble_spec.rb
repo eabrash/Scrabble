ee6e37abfb70f646931e7105d131a99e76f955fe
Emily Abrash <emabrash@gmail.com>
2016-08-30 10:06:59 -0700

    added spec

--- specs/Scrabble_spec.rb
+++ specs/Scrabble_spec.rb
@@ -1,4 +1,10 @@
 require_relative "../lib/Scrabble_scoring"
 require_relative "Spec_helper"
 
-describe 
+describe "Testing Scrabble scoring" do
+
+  it "Letter A is worth one point" do
+    expect(Scrabble::Scoring.score("A").must_equal(1)
+  end
+
+end
