715956ccd511c11866f9ce2bcf60435710205b9d
Erin Bond <erin@erinebond.com>
2016-08-30 10:30:53 -0700

    merge

--- specs/Scrabble_spec.rb
+++ specs/Scrabble_spec.rb
@@ -4,7 +4,7 @@ require_relative "Spec_helper"
 describe "Testing Scrabble scoring" do
 
   it "Letter A is worth one point" do
-    expect(Scrabble::Scoring.score("A").must_equal(1)
+    expect(Scrabble::Scoring.score("A")).must_equal(1)
   end
 
 end
